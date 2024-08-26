cac6be9660656a565c52c0bc25abac350f3e87d8
syndrom3 <80745557+syndrom3@users.noreply.github.com>
2024-08-26 14:09:31 +0700

    Update and rename aosp_grus.mk to orion_grus.mk

--- orion_grus.mk
+++ orion_grus.mk
@@ -9,12 +9,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from grus device
 $(call inherit-product, device/xiaomi/grus/device.mk)
 
-PRODUCT_NAME := aosp_grus
+ORION_MAINTAINER := slaughter
+ORION_MAINTAINER_LINK := t.me/blast_dao
+ORION_BUILD_TYPE := UNOFFICIAL
+ORION_GAPPS := false
+TARGET_ENABLE_BLUR := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_HAS_UDFPS :=  true
+
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
+
+PRODUCT_NAME := orion_grus
 PRODUCT_DEVICE := grus
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
